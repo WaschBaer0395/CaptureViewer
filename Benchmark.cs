2ecffbb932d897c17894b5511e9566a6b717a2bc
WaschBaer0395 <rene-hippe@outlook.com>
2022-05-14 01:29:39 +0200

    Added Benchmark class
    Added a class to show execution time in ms for user defined code
    example:
    using (var bench = new Benchmark($"Execution time in ms:"))
    {
        ... your code here
    }

---
+++ Benchmark.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptureViewer
+{
+    public class Benchmark : IDisposable
+    {
+        private readonly Stopwatch timer = new Stopwatch();
+        private readonly string benchmarkName;
+
+        public Benchmark(string benchmarkName)
+        {
+            this.benchmarkName = benchmarkName;
+            timer.Start();
+        }
+
+        public void Dispose()
+        {
+            timer.Stop();
+            Console.WriteLine($"{benchmarkName} {timer.Elapsed.Milliseconds} ms");
+        }
+    }
+}
